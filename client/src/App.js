d4015cad667db7416f40860422b1cf400abf22c2
stephaneuh <stephane.ris@icloud.com>
2023-06-23 15:26:22 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserList from './components/UserList';
 import UserForm from './components/UserForm';
 
 function App() {
+    const handleSearch = (searchTerm) => {
+        // Logic to handle search functionality and API call
+        console.log('Search term:', searchTerm);
+    };
+
     return (
         <div className="container">
             <h1>User Management</h1>
-            <UserForm />
+            <UserForm handleSearch={handleSearch} />
             <UserList />
         </div>
     );
