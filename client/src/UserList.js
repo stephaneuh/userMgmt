00dd284f27acd240888b6d65d43cc5e8dd110a61
stephaneuh <stephane.ris@icloud.com>
2023-06-23 16:18:45 -0400

    initial commit

--- client/src/UserList.js
+++ client/src/UserList.js
@@ -2,32 +2,41 @@ import React, { useState, useEffect } from 'react';
 
 const UserList = () => {
     const [users, setUsers] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = async () => {
+        try {
+            const apiUrl = 'http://YOUR_IP_ADDRESS:YOUR_PORT/users';
+            const response = await fetch(`${apiUrl}?search=${searchTerm}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Error fetching users');
+            }
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.error('Error fetching users:', error);
+        }
+    };
 
     useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const apiUrl = 'http://YOUR_IP_ADDRESS:YOUR_PORT/users';
-                const response = await fetch(apiUrl, {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                });
-                if (!response.ok) {
-                    throw new Error('Error fetching users');
-                }
-                const data = await response.json();
-                setUsers(data);
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        fetchUsers();
+        handleSearch();
     }, []);
 
     return (
         <div>
             <h1>User List</h1>
+            <div>
+                <input
+                    type="text"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button onClick={handleSearch}>Search</button>
+            </div>
             <ul>
                 {users.map((user) => (
                     <li key={user.id}>{user.name}</li>
