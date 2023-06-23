5356fcc3352bba70ebb8ea2fb2c3cf38fe7f2ed7
stephaneuh <stephane.ris@icloud.com>
2023-06-23 15:29:57 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,23 @@ import UserList from './components/UserList';
 import UserForm from './components/UserForm';
 
 function App() {
-    const handleSearch = (searchTerm) => {
-        // Logic to handle search functionality and API call
-        console.log('Search term:', searchTerm);
+    const [users, setUsers] = useState([]);
+
+    const handleSearch = async (searchTerm) => {
+        try {
+            const response = await fetch(`/users?search=${searchTerm}`);
+            const data = await response.json();
+            setUsers(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
     };
 
     return (
         <div className="container">
             <h1>User Management</h1>
             <UserForm handleSearch={handleSearch} />
-            <UserList />
+            <UserList users={users} />
         </div>
     );
 }
