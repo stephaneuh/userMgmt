e4d0dabc8d362e6ab3e564c91a38a1df33455532
stephaneuh <stephane.ris@icloud.com>
2023-06-23 15:18:34 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import UserList from './components/UserList';
+import UserForm from './components/UserForm';
 
-const App = () => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const [searchQuery, setSearchQuery] = useState('');
-
-    const fetchUsers = async (page = 1, limit = 10) => {
-        try {
-            const response = await axios.get(
-                `/users?page=${page}&limit=${limit}&search=${encodeURIComponent(searchQuery)}`
-            );
-            setUsers(response.data);
-            setTotalPages(response.headers['x-total-pages']);
-            setLoading(false);
-        } catch (error) {
-            console.error('Error fetching users:', error);
-            setError('Error fetching users');
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
-
-    const handlePageChange = (page) => {
-        setCurrentPage(page);
-        fetchUsers(page);
-    };
-
-    const handleSearch = (event) => {
-        event.preventDefault();
-        fetchUsers(1);
-    };
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>{error}</div>;
-    }
-
+function App() {
     return (
-        <div>
-            <h1>User List</h1>
-            <form onSubmit={handleSearch}>
-                <input
-                    type="text"
-                    placeholder="Search by name or email"
-                    value={searchQuery}
-                    onChange={(event) => setSearchQuery(event.target.value)}
-                />
-                <button type="submit">Search</button>
-            </form>
-            <ul>
-                {users.map((user) => (
-                    <li key={user.id}>
-                        Name: {user.name} | Email: {user.email}
-                    </li>
-                ))}
-            </ul>
-            <div>
-                {currentPage > 1 && (
-                    <button onClick={() => handlePageChange(currentPage - 1)}>Previous</button>
-                )}
-                Page {currentPage} of {totalPages}
-                {currentPage < totalPages && (
-                    <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
-                )}
-            </div>
+        <div className="container">
+            <h1>User Management</h1>
+            <UserForm />
+            <UserList />
         </div>
     );
-};
+}
 
 export default App;
